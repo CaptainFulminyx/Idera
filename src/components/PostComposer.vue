fbfa1ba0179580aa5155ef88ea700b40c58bc964
CaptainFulminyx <captainfulminyx@gmail.com>
2026-05-16 00:15:20 +0530

    v forgot 0.026

--- src/components/PostComposer.vue
+++ src/components/PostComposer.vue
@@ -1,18 +1,36 @@
 <template>
+  <div class="writeTextContainer">
+    
   <input class="writeText" placeholder="Write something..." />
+  </div>
 </template>
 <style>
   @import 'tailwindcss';
+.writeTextContainer{
+  width: 100%;
+  height: 120px;
+  position: fixed;
+  bottom:0;
+  display:flex;
+  align-items: center;
+  justify-content: center;
+}
+  
   .writeText{
-    @apply bg-black w-full;
-    
-    border-top-left-radius: 20px;
-    border-top-right-radius: 20px;
-    height: 150px;
+    @apply bg-red-500;
+    border-radius:20px;
+    height: 55px;
+    width: 90%;
+    padding:10px;
     text-transform: 30;
-    position: fixed;
-    bottom:0;
+    opacity:0.6;
     outline:none;
+    transition:opacity 0.3s ;
+  }
+
+  .writeText:hover, 
+  .writeText:focus {
+    opacity: 1;
   }
 </style>
   
